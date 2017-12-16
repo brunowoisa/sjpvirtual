6186112c854ed28095f60bd42b905796732b2e28
Bruno Henrique Woisa <brunowoisa@hotmail.com>
2017-12-16 10:52:14 -0200

    avanços menu guia

--- application/views/Home.php
+++ application/views/Home.php
@@ -223,16 +223,230 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		
 		<!-- Menu Lateral -->
 		<div class="col-md-2" id="menu_guia_comercial">
+			<div class="titulo_item_menu">Guia Comercial</div>
 			<nav>
-				<a href="#"><div class="li">Advocacia</div></a>
-				<a href="#"><div class="li">Agências de Turismo</div></a>
-				<a href="#"><div class="li">Cestas de Café da Manhã</div></a>
-				<a href="#"><div class="li">Item 1</div></a>
-				<a href="#"><div class="li">Item 1</div></a>
-				<a href="#"><div class="li">Item 1</div></a>
-				<a href="#"><div class="li">Item 1</div></a>
-				<a href="#"><div class="li">Item 1</div></a>
-				<a href="#"><div class="li">Item 1</div></a>
+				<a href="#"><div class="item_menu">Advocacia</div></a>
+				<a href="#"><div class="item_menu">Agências de turismo</div></a>
+				<a href="#"><div class="item_menu">limentação</div></a>
+				<a href="#"><div class="item_menu">Açougues</div></a>
+				<a href="#"><div class="item_menu">Bebidas</div></a>
+				<a href="#"><div class="item_menu">Chocolates</div></a>
+				<a href="#"><div class="item_menu">Conveniência</div></a>
+				<a href="#"><div class="item_menu">Distribuidora de Alimentos</div></a>
+				<a href="#"><div class="item_menu">Fábrica de alimentos</div></a>
+				<a href="#"><div class="item_menu">Hortifruti</div></a>
+				<a href="#"><div class="item_menu">Lanchonetes</div></a>
+				<a href="#"><div class="item_menu">Panificadoras e confeitarias</div></a>
+				<a href="#"><div class="item_menu">Peixarias</div></a>
+				<a href="#"><div class="item_menu">Pizzaria</div></a>
+				<a href="#"><div class="item_menu">Produtos Coloniais</div></a>
+				<a href="#"><div class="item_menu">Sorveterias / Açaiterias</div></a>
+				<a href="#"><div class="item_menu">Supermercados</div></a>
+				<a href="#"><div class="item_menu">nimais</div></a>
+				<a href="#"><div class="item_menu">Adestramento</div></a>
+				<a href="#"><div class="item_menu">Agropecuárias</div></a>
+				<a href="#"><div class="item_menu">Hotel para pets</div></a>
+				<a href="#"><div class="item_menu">Pet Shop e Veterinárias</div></a>
+				<a href="#"><div class="item_menu">Artesanatos</div></a>
+				<a href="#"><div class="item_menu">Associações</div></a>
+				<a href="#"><div class="item_menu">Auto escolas</div></a>
+				<a href="#"><div class="item_menu">utomóveis</div></a>
+				<a href="#"><div class="item_menu">Auto elétricas</div></a>
+				<a href="#"><div class="item_menu">Auto peças</div></a>
+				<a href="#"><div class="item_menu">Balanceamento</div></a>
+				<a href="#"><div class="item_menu">Despachantes</div></a>
+				<a href="#"><div class="item_menu">Estética automotiva</div></a>
+				<a href="#"><div class="item_menu">Funilaria</div></a>
+				<a href="#"><div class="item_menu">Garagens</div></a>
+				<a href="#"><div class="item_menu">Lavagem e polimento</div></a>
+				<a href="#"><div class="item_menu">Locação de veículos</div></a>
+				<a href="#"><div class="item_menu">Oficinas mecânicas</div></a>
+				<a href="#"><div class="item_menu">Som e Acessórios</div></a>
+				<a href="#"><div class="item_menu">Aviamentos</div></a>
+				<a href="#"><div class="item_menu">Bancos</div></a>
+				<a href="#"><div class="item_menu">Bares</div></a>
+				<a href="#"><div class="item_menu">Beleza</div></a>
+				<a href="#"><div class="item_menu">Cosméticos</div></a>
+				<a href="#"><div class="item_menu">Podologia</div></a>
+				<a href="#"><div class="item_menu">Salões de beleza</div></a>
+				<a href="#"><div class="item_menu">Bicicletas</div></a>
+				<a href="#"><div class="item_menu">Brindes e presentes</div></a>
+				<a href="#"><div class="item_menu">Brinquedos</div></a>
+				<a href="#"><div class="item_menu">Cestas de Café da manhã</div></a>
+				<a href="#"><div class="item_menu">Combustíveis</div></a>
+				<a href="#"><div class="item_menu">Construção e reforma</div></a>
+				<a href="#"><div class="item_menu">Arquitetos</div></a>
+				<a href="#"><div class="item_menu">Calhas</div></a>
+				<a href="#"><div class="item_menu">Cantoneiras</div></a>
+				<a href="#"><div class="item_menu">Construtoras</div></a>
+				<a href="#"><div class="item_menu">Engenheiros</div></a>
+				<a href="#"><div class="item_menu">Ferragens</div></a>
+				<a href="#"><div class="item_menu">Forros</div></a>
+				<a href="#"><div class="item_menu">Gesso</div></a>
+				<a href="#"><div class="item_menu">Impermeabilizações</div></a>
+				<a href="#"><div class="item_menu">Instalações elétricas</div></a>
+				<a href="#"><div class="item_menu">Instalações hidráulicas</div></a>
+				<a href="#"><div class="item_menu">Interiores</div></a>
+				<a href="#"><div class="item_menu">Lajes</div></a>
+				<a href="#"><div class="item_menu">Licenciamento Ambiental</div></a>
+				<a href="#"><div class="item_menu">Madeireiras</div></a>
+				<a href="#"><div class="item_menu">Marcenarias</div></a>
+				<a href="#"><div class="item_menu">Marmorarias</div></a>
+				<a href="#"><div class="item_menu">Materiais de construção</div></a>
+				<a href="#"><div class="item_menu">Metalúrgicas</div></a>
+				<a href="#"><div class="item_menu">Pintores</div></a>
+				<a href="#"><div class="item_menu">Piscinas</div></a>
+				<a href="#"><div class="item_menu">Pisos</div></a>
+				<a href="#"><div class="item_menu">Portas</div></a>
+				<a href="#"><div class="item_menu">Pré moldados</div></a>
+				<a href="#"><div class="item_menu">Redes de Proteção</div></a>
+				<a href="#"><div class="item_menu">Telhas</div></a>
+				<a href="#"><div class="item_menu">Terraplanagem</div></a>
+				<a href="#"><div class="item_menu">Tintas</div></a>
+				<a href="#"><div class="item_menu">Toldos</div></a>
+				<a href="#"><div class="item_menu">Vidraçarias</div></a>
+				<a href="#"><div class="item_menu">Contabilidade</div></a>
+				<a href="#"><div class="item_menu">Corretoras de Seguro</div></a>
+				<a href="#"><div class="item_menu">Creches</div></a>
+				<a href="#"><div class="item_menu">Decoração</div></a>
+				<a href="#"><div class="item_menu">Cama - Mesa - Banho</div></a>
+				<a href="#"><div class="item_menu">Colchões</div></a>
+				<a href="#"><div class="item_menu">Cortinas</div></a>
+				<a href="#"><div class="item_menu">Lojas de decoração</div></a>
+				<a href="#"><div class="item_menu">Papel de Parede</div></a>
+				<a href="#"><div class="item_menu">Quadros e Molduras</div></a>
+				<a href="#"><div class="item_menu">Telas</div></a>
+				<a href="#"><div class="item_menu">Diversão</div></a>
+				<a href="#"><div class="item_menu">Escolas</div></a>
+				<a href="#"><div class="item_menu">Biblioteca</div></a>
+				<a href="#"><div class="item_menu">Escola de cabelereiros</div></a>
+				<a href="#"><div class="item_menu">Escola de dança</div></a>
+				<a href="#"><div class="item_menu">Escola de idiomas</div></a>
+				<a href="#"><div class="item_menu">Escolas de música</div></a>
+				<a href="#"><div class="item_menu">Escolas particulares</div></a>
+				<a href="#"><div class="item_menu">Escolas profissionalizantes</div></a>
+				<a href="#"><div class="item_menu">Escolas públicas</div></a>
+				<a href="#"><div class="item_menu">Escolas Técnicas</div></a>
+				<a href="#"><div class="item_menu">Supletivo</div></a>
+				<a href="#"><div class="item_menu">Esportes</div></a>
+				<a href="#"><div class="item_menu">Artigos esportivos</div></a>
+				<a href="#"><div class="item_menu">Quadras</div></a>
+				<a href="#"><div class="item_menu">Estofaria</div></a>
+				<a href="#"><div class="item_menu">Faculdades e Universidades</div></a>
+				<a href="#"><div class="item_menu">Festas e eventos</div></a>
+				<a href="#"><div class="item_menu">Artistas e similares</div></a>
+				<a href="#"><div class="item_menu">Buffet</div></a>
+				<a href="#"><div class="item_menu">Decoração de eventos</div></a>
+				<a href="#"><div class="item_menu">Espaço para Festas</div></a>
+				<a href="#"><div class="item_menu">Financiamentos</div></a>
+				<a href="#"><div class="item_menu">Floriculturas</div></a>
+				<a href="#"><div class="item_menu">Games e Eletrônicos</div></a>
+				<a href="#"><div class="item_menu">Gráficas</div></a>
+				<a href="#"><div class="item_menu">Hobby e modelismo</div></a>
+				<a href="#"><div class="item_menu">Hospedagem</div></a>
+				<a href="#"><div class="item_menu">Camping</div></a>
+				<a href="#"><div class="item_menu">Casas de Aluguel</div></a>
+				<a href="#"><div class="item_menu">Casas de Excursões</div></a>
+				<a href="#"><div class="item_menu">Hotéis</div></a>
+				<a href="#"><div class="item_menu">Motel</div></a>
+				<a href="#"><div class="item_menu">Pousadas</div></a>
+				<a href="#"><div class="item_menu">Igrejas</div></a>
+				<a href="#"><div class="item_menu">Imobiliárias</div></a>
+				<a href="#"><div class="item_menu">Informática</div></a>
+				<a href="#"><div class="item_menu">Cartuchos e toners</div></a>
+				<a href="#"><div class="item_menu">Cursos</div></a>
+				<a href="#"><div class="item_menu">Lojas e assistência</div></a>
+				<a href="#"><div class="item_menu">Manutenção de Impressoras</div></a>
+				<a href="#"><div class="item_menu">Sistemas e consultoria</div></a>
+				<a href="#"><div class="item_menu">TI - Tecnologia da Informação</div></a>
+				<a href="#"><div class="item_menu">Internet</div></a>
+				<a href="#"><div class="item_menu">Lan house</div></a>
+				<a href="#"><div class="item_menu">Provedores</div></a>
+				<a href="#"><div class="item_menu">Webdesign</div></a>
+				<a href="#"><div class="item_menu">Jóias</div></a>
+				<a href="#"><div class="item_menu">Jornais e Revistas</div></a>
+				<a href="#"><div class="item_menu">Lotéricas</div></a>
+				<a href="#"><div class="item_menu">Máquinas e Ferramentas</div></a>
+				<a href="#"><div class="item_menu">Motocicletas</div></a>
+				<a href="#"><div class="item_menu">Lojas</div></a>
+				<a href="#"><div class="item_menu">Peças e serviços</div></a>
+				<a href="#"><div class="item_menu">Móveis</div></a>
+				<a href="#"><div class="item_menu">Atacado e fabricação</div></a>
+				<a href="#"><div class="item_menu">Móveis e eletros</div></a>
+				<a href="#"><div class="item_menu">Móveis para escritório</div></a>
+				<a href="#"><div class="item_menu">Móveis usados</div></a>
+				<a href="#"><div class="item_menu">Restauração de Móveis</div></a>
+				<a href="#"><div class="item_menu">Música</div></a>
+				<a href="#"><div class="item_menu">Náutica</div></a>
+				<a href="#"><div class="item_menu">Oticas</div></a>
+				<a href="#"><div class="item_menu">Papelarias</div></a>
+				<a href="#"><div class="item_menu">Prestação de serviços</div></a>
+				<a href="#"><div class="item_menu">Antenas</div></a>
+				<a href="#"><div class="item_menu">Astrologia</div></a>
+				<a href="#"><div class="item_menu">Cartórios</div></a>
+				<a href="#"><div class="item_menu">Chaveiros</div></a>
+				<a href="#"><div class="item_menu">Consultoria</div></a>
+				<a href="#"><div class="item_menu">Dedetizadores</div></a>
+				<a href="#"><div class="item_menu">Eletrônicas</div></a>
+				<a href="#"><div class="item_menu">Extintores</div></a>
+				<a href="#"><div class="item_menu">Fotografia</div></a>
+				<a href="#"><div class="item_menu">Funerárias</div></a>
+				<a href="#"><div class="item_menu">Gás</div></a>
+				<a href="#"><div class="item_menu">Jardinagem</div></a>
+				<a href="#"><div class="item_menu">Lavanderias</div></a>
+				<a href="#"><div class="item_menu">Limpeza</div></a>
+				<a href="#"><div class="item_menu">Mão de Obra</div></a>
+				<a href="#"><div class="item_menu">Noivas</div></a>
+				<a href="#"><div class="item_menu">Pagamentos</div></a>
+				<a href="#"><div class="item_menu">Películas</div></a>
+				<a href="#"><div class="item_menu">Poços Artesianos</div></a>
+				<a href="#"><div class="item_menu">Produtos descartáveis</div></a>
+				<a href="#"><div class="item_menu">Refrigeração</div></a>
+				<a href="#"><div class="item_menu">Segurança e alarme</div></a>
+				<a href="#"><div class="item_menu">Video locadoras</div></a>
+				<a href="#"><div class="item_menu">Propaganda e mídia</div></a>
+				<a href="#"><div class="item_menu">Agências</div></a>
+				<a href="#"><div class="item_menu">Comunicação visual</div></a>
+				<a href="#"><div class="item_menu">Jornais</div></a>
+				<a href="#"><div class="item_menu">Rádios</div></a>
+				<a href="#"><div class="item_menu">Revistas</div></a>
+				<a href="#"><div class="item_menu">Restaurantes</div></a>
+				<a href="#"><div class="item_menu">Setor público</div></a>
+				<a href="#"><div class="item_menu">Shopping Center</div></a>
+				<a href="#"><div class="item_menu">Sindicatos</div></a>
+				<a href="#"><div class="item_menu">Tatuagem e Piercing</div></a>
+				<a href="#"><div class="item_menu">Tele entregas</div></a>
+				<a href="#"><div class="item_menu">Água e Gás</div></a>
+				<a href="#"><div class="item_menu">Comida e bebida</div></a>
+				<a href="#"><div class="item_menu">Remédios</div></a>
+				<a href="#"><div class="item_menu">Tele Entulho</div></a>
+				<a href="#"><div class="item_menu">Telefonia</div></a>
+				<a href="#"><div class="item_menu">Lojas</div></a>
+				<a href="#"><div class="item_menu">Lojas</div></a>
+				<a href="#"><div class="item_menu">Posto telefônico</div></a>
+				<a href="#"><div class="item_menu">Telefonia móvel</div></a>
+				<a href="#"><div class="item_menu">Transportes</div></a>
+				<a href="#"><div class="item_menu">Fretes e mudanças</div></a>
+				<a href="#"><div class="item_menu">Guincho</div></a>
+				<a href="#"><div class="item_menu">Moto-táxi</div></a>
+				<a href="#"><div class="item_menu">Passageiros</div></a>
+				<a href="#"><div class="item_menu">Táxi</div></a>
+				<a href="#"><div class="item_menu">Táxi Aéreo</div></a>
+				<a href="#"><div class="item_menu">Tratores</div></a>
+				<a href="#"><div class="item_menu">Vestuário</div></a>
+				<a href="#"><div class="item_menu">Acessórios</div></a>
+				<a href="#"><div class="item_menu">Bordados</div></a>
+				<a href="#"><div class="item_menu">Calçados</div></a>
+				<a href="#"><div class="item_menu">Costureiras</div></a>
+				<a href="#"><div class="item_menu">Íntima</div></a>
+				<a href="#"><div class="item_menu">Locação de trajes</div></a>
+				<a href="#"><div class="item_menu">Moda infantil</div></a>
+				<a href="#"><div class="item_menu">Moda jovem e adulto</div></a>
+				<a href="#"><div class="item_menu">Pijamas</div></a>
+				<a href="#"><div class="item_menu">Roupas para Festas</div></a>
+				<a href="#"><div class="item_menu">Tecidos</div></a>
+				<a href="#"><div class="item_menu">Uniformes</div></a>
+				<a href="#"><div class="item_menu">Vinhos</div></a>
 			</nav>
 		</div>
 
